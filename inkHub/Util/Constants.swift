49c2c1b44ff601836580ff719e6ad52e873322dd
Kaleb <pomayo@email.arizona.edu>
2018-11-15 23:48:34 -0700

    Sign in ui custom classes complete

---
+++ inkHub/Util/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  inkHub
+//
+//  Created by Kaleb  on 11/15/18.
+//  Copyright © 2018 KMTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
