ddec871cb2fb63fdbfd57a8b3f17f3799345d58d
Kaleb <pomayo@email.arizona.edu>
2018-11-17 18:00:37 -0700

    Added sign out button

--- inkHub/Controller/FeedVC.swift
+++ inkHub/Controller/FeedVC.swift
@@ -13,31 +13,32 @@ import SwiftKeychainWrapper
 class FeedVC: UIViewController {
 
     @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var signOutButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // lower opacity of plus button
+        // lower opacity of buttons
         plusButton.alpha = 0.8
         plusButton.adjustsImageWhenHighlighted = true
+        signOutButton.alpha = 0.8
+        signOutButton.adjustsImageWhenHighlighted = true
         
     }
+    
+    @IBAction func signOutButtonTapped(_ sender: UIButton) {
+        // first remove data from keychain then dismiss VC
+        let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
+        print("CALEB: ID removed from keychain \(keychainResult)")
+        try! Auth.auth().signOut()
+        dismiss(animated: false, completion: nil)
+    }
+    
     
     @IBAction func plusButtonTapped(_ sender: UIButton) {
         
     }
     
-    
-    // TODO: ASSIGN THIS TO THE SIGN OUT BUTTON WHEN U MAKE IT
-    /*
-    @IBAction func signOutTapped(_ sender: Any) {
-        
-        // first remove data from keychain then dismiss VC
-        let keychainResult = KeychainWrapper.standard.remove(key: KEY_UID)
-        print("CALEB: ID removed from keychain \(keychainResult)")
-        try! Auth.auth().signOut()
-        dismiss(animated: false, completion: nil)
-    }
-     */
+
     
     
 
