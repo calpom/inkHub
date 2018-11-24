c3bab6cb699d96e3839d2f47809f09fa86343979
Kaleb <pomayo@email.arizona.edu>
2018-11-24 14:12:51 -0700

    completed sign up and add profile picture

--- inkHub/Controller/SignInVC.swift
+++ inkHub/Controller/SignInVC.swift
@@ -102,43 +102,12 @@ class SignInVC: UIViewController {
                         self.completeSignIn(id: user.user.uid, userData: userData)
                     }
                 } else {
-                    // AT THIS POINT, USER WAS NOT ABLE TO SIGN IN. CURRENTLY IT MAKES EMAIL AUTOMATICALLY BUT INSTEAD
-                    // WE SHOULD PUT IN A SIGN UP BUTTON THAT OPENS A NEW VIEW THAT ALLOWS USER TO CREATE ACCOUNT
-                    
-                    // TODO: MAKE LOADING CIRCLE AND BLACK BACKGROUND DISAPPEAR
-                    
-                    
-                    // TODO: MAKE WARNING LABEL APPEAR AND MAKE IT SAY
-                    // PLEASE TRY AGAIN
+                    // AT THIS POINT, USER WAS NOT ABLE TO SIGN IN.
+                    self.warningLabel.text = "User not found"
+                    self.warningLabel.isHidden = false
+                    return
                     
                     
-                    // TODO: ADD A SIGN UP BUTTON THAT USES BELOW METHOD AND MAKES SURE "CONFIRM PASSWORD" FIELD IS THERE
-                    
-                    
-                    // THIS IS THE METHOD THAT CREATES USER (USE IT FOR ABOVE TODO)
-                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
-                        if error != nil {
-                            
-                            // TODO: MAKE LOADING CIRCLE AND BLACK BACKGROUND DISAPPEAR
-                            
-                            // AT THIS POINT, inkHub COULD NOT CREATE ACCOUNT ? MAYBE BECAUSE EMAIL ALREADY EXISTS
-                            print("CALEB: Unable to authenticate with Firebase using email")
-                            
-                            // TODO: IN THE SIGN UP VIEW, MAKE A LABEL APPEAR THAT SAYS "PLEASE TRY ANOTHER EMAIL"
-                            
-                            
-                        } else {
-                            
-                            // TODO: MAKE LOADING CIRCLE AND BLACK BACKGROUND DISAPPEAR
-                            
-                            print("CALEB: Successfully authenticated with Firebase using email")
-                            if let user = user {
-                                let userData = ["provider": user.user.providerID]
-                                self.completeSignIn(id: user.user.uid, userData: userData)
-                            }
-                            
-                        }
-                    })
                 }
             })
         }
--- inkHub/Controller/SignUpVC.swift
+++ inkHub/Controller/SignUpVC.swift
@@ -11,27 +11,77 @@ import Firebase
 import SwiftKeychainWrapper
 
 class SignUpVC: UIViewController {
+    
+    var selectedImage: UIImage!
 
     @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var emailAdressField: FancyField!
     @IBOutlet weak var usernameField: FancyField!
     @IBOutlet weak var passwordField: FancyField!
     @IBOutlet weak var passwordField2: FancyField!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         warningLabel.isHidden = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SignUpVC.handleSelectProfileImageView))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         warningLabel.isHidden = true
     }
     
+    
+    
+    @objc func handleSelectProfileImageView() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+    }
+    
     @IBAction func signUpButtonTapped(_ sender: Any) {
         
-        /*
+        if let email = self.emailAdressField.text,
+            let userName = self.usernameField.text,
+            let pwd = self.passwordField.text,
+            let pwd2 = self.passwordField2.text{
+            
+            if pwd.count < 6 {
+                // TODO: make warning label appear and make it say
+                // "password must be at least 6 characters long"
+                print("CALEB: Password is too short")
+                warningLabel.text = "Password must be at least 6 characters long"
+                warningLabel.isHidden = false
+                return
+            }
+            
+            
+            // DO ERROR CHECKS HERE
+            
+            // TODO make sure pasword and password2 are equal
+            if pwd != pwd2 {
+                print("CALEB: Passwords are not equal")
+                warningLabel.text = "Passwords are not equal"
+                warningLabel.isHidden = false
+                return
+            }
+            // TODO make sure userName is not taken
+            
+            
+            
+            
+            
+            
+            
+            
         // THIS IS THE METHOD THAT CREATES USER (USE IT FOR ABOVE TODO)
         Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
             if error != nil {
@@ -42,6 +92,9 @@ class SignUpVC: UIViewController {
                 print("CALEB: Unable to authenticate with Firebase using email")
                 
                 // TODO: IN THE SIGN UP VIEW, MAKE A LABEL APPEAR THAT SAYS "PLEASE TRY ANOTHER EMAIL"
+                self.warningLabel.text = "Please try another email"
+                self.warningLabel.isHidden = false
+                return
                 
                 
             } else {
@@ -49,17 +102,82 @@ class SignUpVC: UIViewController {
                 // TODO: MAKE LOADING CIRCLE AND BLACK BACKGROUND DISAPPEAR
                 
                 print("CALEB: Successfully authenticated with Firebase using email")
+                /*
                 if let user = user {
-                    let userData = ["provider": user.user.providerID]
+                    let userData = ["provider": user.user.providerID, "name": userName]
                     self.completeSignIn(id: user.user.uid, userData: userData)
                 }
+                */
+                
+                let uid = user?.user.uid
+                let storageRef = Storage.storage().reference(forURL: "gs://inkhub1.appspot.com").child("profile-pics").child(uid!)
+                if let profileImg = self.selectedImage,
+                    let imageData = profileImg.jpegData(compressionQuality: 0.1) {
+                    
+                    
+                    storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                        if error != nil {
+                            return
+                        }
+                        
+                        storageRef.downloadURL(completion: { (metadata, error) in
+                            if error != nil {
+                                return
+                            }
+                            
+                            let profileImageUrl = metadata?.absoluteString
+                            let ref = Database.database().reference()
+                            let usersReference = ref.child("users")
+                            let newUserReference = usersReference.child(uid!)
+                            newUserReference.setValue(["name": userName, "email": email, "profileImageUrl": profileImageUrl])
+                            KeychainWrapper.standard.set((user?.user.uid)!, forKey: KEY_UID)
+                            self.performSegue(withIdentifier: "goToFeed", sender: nil)
+                        })
+                    })
+                    
+                    
+                    /*
+                    storageRef.downloadURL(completion: { (metadata, error) in
+                        if error != nil {
+                            return
+                        }
+                        
+                        let profileImageUrl = metadata?.absoluteString
+                        let ref = Database.database().reference()
+                        let usersReference = ref.child("users")
+                        let newUserReference = usersReference.child(uid!)
+                        newUserReference.setValue(["name": userName, "email": email, "profileImageUrl": profileImageUrl])
+                        KeychainWrapper.standard.set((user?.user.uid)!, forKey: KEY_UID)
+                        self.performSegue(withIdentifier: "goToFeed", sender: nil)
+                    })
+                     */
+                    
+                }
+                
+                
+                
                 
             }
+            
+            
+            
+            
+            
         })
-         */
+        
+        }
+        
+        
     }
     
     
+    // dont think im even using this method in this class
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
+        let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
+        performSegue(withIdentifier: "goToFeed", sender: nil)
+        print("CALEB: Data saved to keychain \(keychainResult)")
+    }
     
     
     
@@ -68,4 +186,16 @@ class SignUpVC: UIViewController {
     }
     
 
+}
+
+
+extension SignUpVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            selectedImage = image
+            profileImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
